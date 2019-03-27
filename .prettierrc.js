0a9965b9b37bb8ce6253b6b1df03cf115d3c9bab
maccliu <maccliu@qq.com>
2019-03-27 20:36:29 +0800

    added .npmrc
    使用淘宝的npm镜像，加速npm install

---
+++ .prettierrc.js
@@ -0,0 +1,28 @@
+/**
+ * prettier代码格式化配置
+ * 配置文件 https://prettier.io/docs/en/configuration.html
+ * 配置说明 https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  // 最大行宽
+  printWidth: 120,
+
+  // tab
+  useTabs: false,
+  tabWidth: 2,
+
+  // 单引号/双引号
+  singleQuote: true,
+
+  // 行结尾是否用分号
+  semi: true,
+
+  // 结尾逗号
+  trailingComma: 'es5',
+
+  // 大括号内加空格
+  bracketSpacing: false,
+
+  // 行结束符
+  endOfLine: 'lf',
+};
